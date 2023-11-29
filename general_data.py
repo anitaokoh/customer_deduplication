e9211a6d953df2d95e68b8c0bb9ee849ad5da169
anitaokoh <sleekymeeratura@gmail.com>
2023-11-29 16:40:14 +0100

    chore: standarize the phone number format

--- general_data.py
+++ general_data.py
@@ -19,7 +19,7 @@ def transform_phone_number(phone_number):
     str: The transformed phone number with specified characters replaced or removed.
     """
     # Combined regex for replacing +49(0) with 00, removing brackets, and removing spaces
-    return re.sub(r'\+49\(0\)|[()]|\s+', '', phone_number)
+    return re.sub(r'\+49\(0\)|[()]|\s+|\+49', '', phone_number)
 
 def generate_database(number):
     """
